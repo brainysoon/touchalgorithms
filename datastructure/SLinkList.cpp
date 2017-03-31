29cec1a6ef4e356ddddf4bf97aa8ccfcd42e51db
brainysoon <brainysoon@gmail.com>
2017-03-31 12:14:34 +0800

    add-slink-list-malloc

--- datastructure/SLinkList.cpp
+++ datastructure/SLinkList.cpp
@@ -21,6 +21,17 @@ int InitSpace_SL(SLinkList &space){
 	return OK; 
 } 
 
+//从备用链表里面提取一个节点，入股为空 则返回0 否则返回节点下标 
+int Malloc_SL(SLinkList &space){
+	
+	int i=space[0].cur;		//备用链表的第一个元素 
+	
+	if (space[0].cur)		//如果不为空则将备用链表后移一位 
+	space[0].cur=space[i].cur;		
+
+	return i;			//返回提取节点的下标 
+}//Malloc_SL
+
 int main(){
 	
 	//初始化一个静态链表 
@@ -30,5 +41,9 @@ int main(){
 	//打印链接成备用链表后的链表
 	for (int i=0;SL[i].cur; i=SL[i].cur){
 		printf("-->%d\n",SL[i].cur);
-	} 
+	}
+	
+	//从备用链表里面提取一个节点
+	int i=Malloc_SL(SL);
+	printf("BackUP:%d\n",i); 
 } 
